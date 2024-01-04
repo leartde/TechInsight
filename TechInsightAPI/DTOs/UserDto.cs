54216f2a6803fd234635e9b09eb4da43ac79249c
Drin Vitia <dv58191@ubt-uni.net>
2024-01-04 16:31:38 +0100

    UserDto

---
+++ TechInsightAPI/DTOs/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using TechInsightAPI.Models;
+using TechInsightAPI.DTOs;
+
+namespace TechInsightAPI.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string ProfilePicUrl { get; set; }
+        public UserRole UserRole { get; set; }
+        public DateTime RegistrationTime { get; set; }
+    }
+}
