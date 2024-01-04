695984ce177cfa34ad71799facec3b349c467085
Drin Vitia <117443499+DrinVitia@users.noreply.github.com>
2024-01-04 20:52:58 +0100

    footer

--- TechInsightClient/src/App.jsx
+++ TechInsightClient/src/App.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import "./index.css";
 import "./app.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 const App = () => {
     return ( 
         <>
         <Navbar/>
         <Outlet/>
+        <Footer/>
         </>
     );
 }
