2e8bbb30215c0fe7de819f09ca639235125db0dd
Drin Vitia <117443499+DrinVitia@users.noreply.github.com>
2024-01-05 23:01:16 +0100

    Refactor CategorySelector and add async to handleSubmit in SignUp and in LogIn

--- TechInsightClient/src/components/Login.jsx
+++ TechInsightClient/src/components/Login.jsx
@@ -1,61 +1,92 @@
 import React, { useState } from 'react';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch('https://localhost:7265/api/User/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Email: email,
+          Password: password,
+        }),
+      });
+  
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        setError(`Login failed: ${errorResponse.title}`);
+        return;
+      }
+  
+
+      console.log('Login successful');
+      setError('');
+  
+
+    } catch (error) {
+      console.error('Error:', error);
+      setError('An error occurred while processing your request.');
+    }
+  };
+  
+  
 
     return (
-        <div class="flex items-center justify-center h-screen max-h-screen">
-            <div class="relative mx-auto w-full max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
-                <div class="w-full">
-                    <div class="text-center">
-                        <h1 class="text-3xl font-semibold text-gray-900">Sign in</h1>
-                        <p class="mt-2 text-gray-500">Sign in below to access your account</p>
+        <div className="flex items-center justify-center h-screen max-h-screen">
+            <div className="relative mx-auto w-full max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
+                <div className="w-full">
+                    <div className="text-center">
+                        <h1 className="text-3xl font-semibold text-gray-900">Sign in</h1>
+                        <p className="mt-2 text-gray-500">Sign in below to access your account</p>
                     </div>
-                    <div class="mt-5">
+                    <div className="mt-5">
                         <form onSubmit={handleSubmit}>
-                            <div class="relative mt-6">
+                            <div className="relative mt-6">
                                 <input 
                                     type="email" 
                                     name="email" 
                                     id="email" 
                                     placeholder="Email Address" 
-                                    class="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" 
-                                    autocomplete="NA" 
+                                    className="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" 
+                                    autoComplete="NA" 
                                     value={email}
                                     onChange={handleEmailChange}
                                 />
-                                <label for="email" class="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Email Address</label>
+                                <label htmlFor="email" className="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Email Address</label>
                             </div>
-                            <div class="relative mt-6">
+                            <div className="relative mt-6">
                                 <input 
                                     type="password" 
                                     name="password" 
                                     id="password" 
                                     placeholder="Password" 
-                                    class="peer peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" 
+                                    className="peer peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" 
                                     value={password}
                                     onChange={handlePasswordChange}
                                 />
-                                <label for="password" class="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Password</label>
+                                <label htmlFor="password" className="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Password</label>
                             </div>
-                            <div class="my-6">
-                                <button type="submit" class="w-full rounded-md bg-indigo-500 px-3 py-4 text-white focus:bg-gray-600 focus:outline-none">Sign in</button>
+                            <div className="my-6">
+                                <button type="submit" className="w-full rounded-md bg-indigo-500 px-3 py-4 text-white focus:bg-gray-600 focus:outline-none">Sign in</button>
                             </div>
-                            <p class="text-center text-sm text-gray-500">Don't have an account yet?
-                                <a href="/signup" class="font-semibold text-blue-500 hover:underline focus:text-gray-800 focus:outline-none">Sign up</a>.
+                            <p className="text-center text-sm text-gray-500">Don't have an account yet?
+                                <a href="/signup" className="font-semibold text-blue-500 hover:underline focus:text-gray-800 focus:outline-none">Sign up</a>.
                             </p>
                         </form>
                     </div>
