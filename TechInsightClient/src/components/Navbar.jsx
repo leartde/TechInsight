b5a641ddee89bf55701ce239e00646edac001093
leartde <leartdell@gmail.com>
2024-01-09 12:43:44 +0100

    added avatar on navbar with dropdown menu

--- TechInsightClient/src/components/Navbar.jsx
+++ TechInsightClient/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink, useNavigate } from "react-router-dom";
 import { FaBars, FaFacebook, FaXmark } from "react-icons/fa6";
 import { FaDribbble} from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa6";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Cookies from "universal-cookie";
 
 const Navbar = () => {
@@ -10,6 +10,24 @@ const Navbar = () => {
     const navigate = useNavigate();
   const cookies = new Cookies();
   const token = cookies.get("token");
+  const [isAvatarOpen, setIsAvatarOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const closeDropdown = () => {
+    setIsAvatarOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        closeDropdown();
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
 
     console.log(isMenuOpen);
@@ -40,23 +58,56 @@ const Navbar = () => {
                 </ul>
                 {/* {menu icons} */}
                 <div className="text-gray-600 md:flex gap-4 items-center hidden">
-                    {/* <a href="https://www.facebook.com" className="hover:text-[#009bd6]"> <FaFacebook/> </a>
-                    <a href="https://dribbble.com" className="hover:text-[#009bd6]"> <FaDribbble/> </a>
-                    <a href="https://x.com/" className="hover:text-[#009bd6]"> <FaTwitter/> </a> */}
-    
-                   {
-                    token? <button onClick={()=>{
-                        cookies.remove("token");
-                        navigate("/")
-                    }} className="bg-[#009bd6] px-6 py-2 font-medium rounded hover:bg-[#4597b8]
-                    transition-all duration-20 ease-in text-white
-    
-                    " > Logout</button>: <Link to='/login' className="bg-[#009bd6] px-6 py-2 font-medium rounded hover:bg-[#4597b8]
-                    transition-all duration-20 ease-in text-white
-    
-                    " > Log In</Link>
-                   }
-                </div>
+  {token ? (
+    <div className="relative">
+      <img
+        id="avatarButton"
+        type="button"
+        data-dropdown-toggle="userDropdown"
+        data-dropdown-placement="bottom-start"
+        ref={dropdownRef}
+        className="w-10 h-10 rounded-full cursor-pointer"
+        src={token.profilePicUrl}
+        alt="User dropdown"
+        onClick={() => setIsAvatarOpen(!isAvatarOpen)}
+        onClickAway={() => setIsAvatarOpen(!isAvatarOpen)}
+        
+      />
+
+      <div
+        id="userDropdown"
+        className={' absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg  ring-1 ring-black ring-opacity-5 ' + (isAvatarOpen ? 'block' : 'hidden')}
+      >
+        <div className="px-4 py-3 text-sm text-gray-900 ">
+          <div>{token.username}</div>
+          <div className="font-medium truncate">{token.email}</div>
+        </div>
+
+        <div className="py-2 ">
+            <Link to={`/profile/${token.id}`} className="block px-4 py-2 text-sm text-gray-700   hover:bg-gray-600  hover:text-white"> Profile Page </Link>
+        </div>
+
+        <div className="py-1  w-full">
+          <button onClick={() => {
+            cookies.remove("token");
+            navigate("/");
+            setIsAvatarOpen(false);
+          }}
+            className="w-full text-left px-4   py-2 text-sm text-gray-700 hover:bg-gray-600  dark:hover:text-white">
+            Sign out
+          </button>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Link
+      to="/login"
+      className="bg-[#009bd6] px-6 py-2 font-medium rounded hover:bg-[#4597b8] transition-all duration-20 ease-in text-white"
+    >
+      Log In
+    </Link>
+  )}
+</div>
     
                 {/* mobile menu button */}
                 <div className="text-gray-400 md:hidden">
