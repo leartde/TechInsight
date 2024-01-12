2983421c3818ae567640444a4874bfd5a6c9bcf7
Drin Vitia <dv58191@ubt-uni.net>
2024-01-12 17:31:52 +0100

    Refactor BlogCard2 and ProfileCard components

--- TechInsightClient/src/components/BlogCard2.jsx
+++ TechInsightClient/src/components/BlogCard2.jsx
@@ -1,52 +1,38 @@
-
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { FaDesktop, FaFontAwesome, FaGear } from 'react-icons/fa6';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import addClick from '../Services.jsx/AddClick';
 
-
 const BlogCard2 = ({ blogs, currentPage, selectedCategory, pageSize, user }) => {
-
   const filteredBlogs = blogs
     .filter((blog) => !selectedCategory || blog.category === selectedCategory)
     .slice((currentPage - 1) * pageSize, currentPage * pageSize);
 
   return (
-    <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8'>
-      {filteredBlogs.map((blog) => (
-        <div key={blog.id} className="max-w-sm rounded-xl max-h-80 bg-white p-3 pb-5 shadow-sm overflow-hidden">
-          <Link
-            to={`/blogs/${blog.id}`}
-            onClick={() => addClick(blog.id, user.id)}
-            className="block rounded-md overflow-hidden"
-          >
-            <img
-              src={blog.imageURL}
-              className="w-full h-40 object-cover hover:scale-110 transition duration-500"
-              alt={blog.title}
-            />
-          </Link>
-          <div className="mt-3">
-            <h2 className="break-words block text-base font-semibold text-gray-700 hover:text-blue-500 transition font-roboto">
-              {blog.title}
-            </h2>
-            <div className="mt-2 flex space-x-3">
-              <Link to={`/profile/${blog.userId}`}>
-                <div className="flex text-gray-600 text-md items-center">
-                  <span className="mr-1 text-xs">
-                    <img src={blog.userImage} className='h-8 w-8 rounded-full' alt={blog.author} />
-                  </span>
-                  {blog.author}
-                </div>
-              </Link>
-              <div className="flex text-gray-400 text-xs items-center">
-                {new Date(blog.createdAt).toLocaleDateString()}
+    <div className="max-w-7xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12 max-md:max-w-lg mx-auto">
+        {filteredBlogs.map((blog) => (
+          <div key={blog.id} className="bg-white cursor-pointer rounded overflow-hidden group relative before:absolute before:inset-0 before:z-10 before:opacity-50">
+            <Link
+              to={`/blogs/${blog.id}`}
+              onClick={() => addClick(blog.id, user.id)}
+            >
+              <img
+                src={blog.imageURL}
+                className="w-full h-64 object-cover group-hover:scale-110 transition-all duration-300"
+                alt={blog.title}
+              />
+            </Link>
+            <div className="p-6 absolute bottom-0 left-0 right-0 z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gray-500 bg-opacity-50">
+              <span className="text-sm block mb-2 text-white font-semibold">{new Date(blog.createdAt).toLocaleDateString().toUpperCase()} | BY {blog.author}</span>
+              <h3 className="text-xl font-bold text-white">{blog.title}</h3>
+              <div className="mt-4">
+                <p className="text-gray-200 text-sm">{blog.description}</p>
+                <p className="text-gray-200 text-sm mt-2">{blog.content}</p>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- TechInsightClient/src/components/ProfileComponents/ProfileCard.jsx
+++ TechInsightClient/src/components/ProfileComponents/ProfileCard.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaFacebook, FaLinkedin, FaInstagram, FaGithub } from 'react-icons/fa';
 
 const ProfileCard = ({user}) => {
   return (
-    
-
-<div class="w-2/3  max-w-sm bg-white border-gray-200 rounded-lg shadow-md ">
-   
-    <div class="flex flex-col items-center pb-10 pt-6">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={user.profilePicUrl} alt="Bonnie image"/>
-        <h5 class="mb-1 text-xl font-medium text-[#009bd6]">{user.username}</h5>
-        <span class="text-sm text-center text-gray-800 dark:text-gray-400">{user.bio}</span>
-        <div class="flex mt-4 md:mt-6">
-            <Link to={`/profile/${user.id}`} className="inline-flex items-center justify-center py-2 text-sm font-medium text-center text-white border border-transparent rounded-full px-5 bg-[#009bd6] hover:bg-opacity-90">View Profile</Link>
+    <div className="bg-white p-4 shadow-[0_2px_15px_-6px_rgba(0,0,0,0.2)] w-full max-w-xs rounded-2xl font-[sans-serif] overflow-hidden mx-auto mt-4">
+      <div className="flex flex-col items-center">
+        <img src={user.profilePicUrl} className="w-24 h-w-24 rounded-full" alt="User image" />
+        <div className="mt-4 text-center">
+          <p className="text-lg text-[#333] font-extrabold">{user.username}</p>
+          <p className="text-sm text-gray-500 mt-2">{user.bio}</p>
         </div>
+      </div>
+      <div className="mt-6 flex justify-between">
+        <div className="bg-gray-100 hover:bg-gray-200 w-10 h-10 p-2 flex items-center justify-center rounded-full cursor-pointer mx-1">
+          <FaFacebook size={16} color='#000000' />
+        </div>
+        <div className="bg-gray-100 hover:bg-gray-200 w-10 h-10 p-2 flex items-center justify-center rounded-full cursor-pointer mx-1">
+          <FaLinkedin size={16} color='#000000' />
+        </div>
+        <div className="bg-gray-100 hover:bg-gray-200 w-10 h-10 p-2 flex items-center justify-center rounded-full cursor-pointer mx-1">
+          <FaInstagram size={16} color='#000000' />
+        </div>
+        <div className="bg-gray-100 hover:bg-gray-200 w-10 h-10 p-2 flex items-center justify-center rounded-full cursor-pointer mx-1">
+          <FaGithub size={16} color='#000000' />
+        </div>
+      </div>
+      <div className="flex mt-4 md:mt-6 justify-center">
+        <Link to={`/profile/${user.id}`} className="inline-flex items-center justify-center py-2 text-sm font-medium text-center text-white border border-transparent rounded-full px-4 bg-[#009bd6] hover:bg-opacity-90">View Profile</Link>
+      </div>
     </div>
-</div>
-
   )
 }
 
