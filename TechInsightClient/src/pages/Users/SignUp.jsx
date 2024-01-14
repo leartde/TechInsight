f19dff2da666c309d264db1900c47dcab9d084dd
Drin Vitia <dv58191@ubt-uni.net>
2024-01-14 17:20:08 +0100

    fix

--- TechInsightClient/src/pages/Users/SignUp.jsx
+++ TechInsightClient/src/pages/Users/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import GoogleLogo from '../../assets/Google__G__logo.svg.png';
 import GitHubLogo from '../../assets/github-mark-white.png';
 import { ToastContainer, toast } from 'react-toastify';
@@ -10,39 +10,9 @@ const SignUp = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [username, setUsername] = useState('');
-    const [user, setUser] = useState(null);
-    const [signupStatus, setSignupStatus] = useState(null);
     const [emailError, setEmailError] = useState('');
     const [usernameError, setUsernameError] = useState('');
     const navigate = useNavigate();
-    const passRegex = '^(?=.*[0-9]).{8,}$';
-
-    // useEffect(() => {
-    //     const signup = async () => {
-    //         try {
-    //             const response = await fetch('https://localhost:7265/api/User/signup', {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                 },
-    //                 body: JSON.stringify(user),
-    //             });
-    //             if (response.ok) {
-    //                 const data = await response.json();
-    //                 toast.success('You have successfully signed up!');
-    //             } else {
-    //                 console.log('Error:', response.statusText);
-    //                 setSignupStatus('Error');
-    //             }
-    //         } catch (error) {
-    //             console.error('Error:', error);
-    //             setSignupStatus('Error');
-    //         }
-    //     };
-    //     if (user) {
-    //         signup();
-    //     }
-    // }, [user]);
 
     const handleUsernameChange = (e) => {
         const enteredUsername = e.target.value;
@@ -73,26 +43,28 @@ const SignUp = () => {
         setConfirmPassword(e.target.value);
     };
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
         if (!username || !email || !password || !confirmPassword) {
             console.error('Please fill in all the required fields.');
-            toast.error("Please fill in all the required fields.");
+            toast.error('Please fill in all the required fields.');
             return;
         }
-    
+
         if (emailError || usernameError) {
             console.error('Invalid input. Please correct the errors.');
-            toast.error("Invalid input. Please correct the errors.");
+            toast.error('Invalid input. Please correct the errors.');
             return;
         }
-    
+
         const userData = {
             Username: username,
             Email: email,
             Password: password,
             ConfirmPassword: confirmPassword,
         };
-    
+
         try {
             const response = await fetch('https://localhost:7265/api/User/signup', {
                 method: 'POST',
@@ -101,21 +73,20 @@ const SignUp = () => {
                 },
                 body: JSON.stringify(userData),
             });
-    
+
             if (response.ok) {
                 console.log('User registered successfully');
-                toast.success("User registered successfully");
+                toast.success('User registered successfully');
                 navigate('/login');
             } else {
                 console.error('Error during registration:', response.statusText);
-                toast.error("Error during registration. Please try again.");
+                toast.error('Error during registration. Please try again.');
             }
         } catch (error) {
             console.error('Error during registration:', error);
-            toast.error("Error during registration. Please try again.");
+            toast.error('Error during registration. Please try again.');
         }
     };
-    
 
     return (
         <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center items-center mt-12">
@@ -123,17 +94,12 @@ const SignUp = () => {
             <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
                 <div className="lg:w-1/3 xl:w-4/12 p-3 sm:p-6">
                     <div className="text-center">
-                        <a
-                            href="/"
-                            className="w-32 mx-auto text-3xl font-bold text-black"
-                        >
+                        <a href="/" className="w-32 mx-auto text-3xl font-bold text-black">
                             Tech<span className="text-[#009bd6]">Insight</span>
                         </a>
                     </div>
                     <div className="mt-12 flex flex-col items-center">
-                        <h1 className="text-2xl xl:text-3xl font-extrabold">
-                            Sign up
-                        </h1>
+                        <h1 className="text-2xl xl:text-3xl font-extrabold">Sign up</h1>
                         <div className="w-full flex-1 mt-8 text-sm">
                             <div className="flex flex-col items-center">
                                 <button
@@ -148,9 +114,7 @@ const SignUp = () => {
                                             />
                                         </a>
                                     </div>
-                                    <span className="ml-4">
-                                        Sign Up with Google
-                                    </span>
+                                    <span className="ml-4">Sign Up with Google</span>
                                 </button>
 
                                 <button
@@ -163,9 +127,7 @@ const SignUp = () => {
                                             className="w-6 h-6"
                                         />
                                     </div>
-                                    <span className="ml-4">
-                                        Sign Up with GitHub
-                                    </span>
+                                    <span className="ml-4">Sign Up with GitHub</span>
                                 </button>
                             </div>
 
@@ -176,55 +138,53 @@ const SignUp = () => {
                             </div>
 
                             <div className="mx-auto max-w-xs">
-                                <input
-                                    className={`w-full px-6 py-3 mb-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-xs focus:outline-none focus:border-gray-400 focus:bg-white ${
-                                        usernameError ? 'border-red-500' : ''
-                                    }`}
-                                    type="name"
-                                    placeholder="Username"
-                                    value={username}
-                                    onChange={handleUsernameChange}
-                                />
-                                {usernameError && (
-                                    <p className="text-xs text-red-500 mt-1">
-                                        {usernameError}
-                                    </p>
-                                )}
-                                <input
-                                    className={`w-full px-6 py-3 mb-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-xs focus:outline-none focus:border-gray-400 focus:bg-white ${
-                                        emailError ? 'border-red-500' : ''
-                                    }`}
-                                    type="email"
-                                    placeholder="Email"
-                                    value={email}
-                                    onChange={handleEmailChange}
-                                />
-                                {emailError && (
-                                    <p className="text-xs text-red-500 mt-1">
-                                        {emailError}
-                                    </p>
-                                )}
-                                <input
-                                    className="w-full px-6 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-xs focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                                    type="password"
-                                    placeholder="Password"
-                                    value={password}
-                                    onChange={handlePasswordChange}
-                                />
-                                <input
-                                    className="w-full px-6 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-xs focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                                    type="password"
-                                    placeholder="Confirm Password"
-                                    value={confirmPassword}
-                                    onChange={handleConfirmPasswordChange}
-                                />
+                                <form onSubmit={handleSubmit}>
+                                    <input
+                                        className={`w-full px-6 py-3 mb-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-xs focus:outline-none focus:border-gray-400 focus:bg-white ${
+                                            usernameError ? 'border-red-500' : ''
+                                        }`}
+                                        type="name"
+                                        placeholder="Username"
+                                        value={username}
+                                        onChange={handleUsernameChange}
+                                    />
+                                    {usernameError && (
+                                        <p className="text-xs text-red-500 mt-1">{usernameError}</p>
+                                    )}
+                                    <input
+                                        className={`w-full px-6 py-3 mb-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-xs focus:outline-none focus:border-gray-400 focus:bg-white ${
+                                            emailError ? 'border-red-500' : ''
+                                        }`}
+                                        type="email"
+                                        placeholder="Email"
+                                        value={email}
+                                        onChange={handleEmailChange}
+                                    />
+                                    {emailError && (
+                                        <p className="text-xs text-red-500 mt-1">{emailError}</p>
+                                    )}
+                                    <input
+                                        className="w-full px-6 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-xs focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                        type="password"
+                                        placeholder="Password"
+                                        value={password}
+                                        onChange={handlePasswordChange}
+                                    />
+                                    <input
+                                        className="w-full px-6 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-xs focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                        type="password"
+                                        placeholder="Confirm Password"
+                                        value={confirmPassword}
+                                        onChange={handleConfirmPasswordChange}
+                                    />
 
-                                <button
-                                    className="mt-5 tracking-wide font-semibold bg-[#009bd6] text-gray-100 w-full py-4 rounded-lg hover:bg-[#4597b8] transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                                    onClick={handleSubmit}
-                                >
-                                    <span className="ml-3">Sign Up</span>
-                                </button>
+                                    <button
+                                        type="submit"
+                                        className="mt-5 tracking-wide font-semibold bg-[#009bd6] text-gray-100 w-full py-4 rounded-lg hover:bg-[#4597b8] transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                                    >
+                                        <span className="ml-3">Sign Up</span>
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div>
